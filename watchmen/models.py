e4844772deca672b81b0dfcf37b388d6718be62d
Sudipta Pandit <realsdx@protonmail.com>
2020-10-22 22:48:28 +0530

    add: models for Videos

--- watchmen/models.py
+++ watchmen/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+
+class Video(models.Model):
+    video_id = models.CharField(max_length=16, unique=True, db_index=True)
+    title = models.CharField(max_length=1024)
+    description = models.TextField(blank=True, null=True)
+    published_at = models.DateTimeField()
+    thumbnail_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
